d0316f961244f4726e711e5df31642770d0df4fc
StefanLiu007 <1319328007@qq.com>
2016-09-07 13:34:10 +0800

    add password_manager and tshield

--- eden/WebContent/js/page.js
+++
@@ -1,7 +0,0 @@
-//@param requestURL
-function gotoPage(url,currentPage,pageSize,searchContent){ 
-//	alert("kkk");
-	//alert(url+","+currentPage+","+pageSize);
-//	if(true) return;
-	window.location.href = url + "?searchContent="+searchContent+"&page="+currentPage+"&pagesize="+pageSize;
-}
